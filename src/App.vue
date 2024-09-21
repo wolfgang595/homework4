ec19b05f3ea0722a8693981790096f0129b2516f
Nathan Serna <nath2534@outlook.com>
2024-09-21 13:48:55 -0500

    taking break

--- src/App.vue
+++ src/App.vue
@@ -2,6 +2,7 @@
 import Header from './components/Header.vue';
 import Balance from './components/Balance.vue';
 import IncomeExpenses from './components/IncomeExpenses.vue';
+import AddTransaction from './components/AddTransaction.vue';
 
 
   import {ref, computed} from 'vue'
@@ -62,6 +63,7 @@ const transactions = ref([
 <div class="container">
   <Balance :total="sum"></Balance>
   <IncomeExpenses :income="moneyIn" :expense="moneyOut"></IncomeExpenses>
+  <AddTransaction></AddTransaction>
 
 
 </div>
---
+++ src/components/AddTransaction.vue
@@ -0,0 +1,56 @@
+<script setup>
+
+
+
+
+
+</script>
+
+
+<template>
+
+
+    <h3>Add a new transaction</h3>
+    <form id="form" @submit.prevent="onSubmit">
+
+        <div class="form-control">
+            <label for="text">Enter transaction</label>
+            <input type="text" id="text" v-model="text" placeholder="Enter Transaction...">
+        </div>
+
+
+
+
+        <div class="form-control">
+            <label for="amount">Enter transaction Amount</label>
+            <input type="text" id="amount" v-model="amount" placeholder="Enter Negative Value for Expenses">
+        </div>
+
+
+
+
+
+
+    </form>
+
+
+</template>
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
