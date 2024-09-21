cc1ed7da53f0c32c5c0d367b71f1d299c200be29
Nathan Serna <nath2534@outlook.com>
2024-09-20 23:00:05 -0500

    added header and balance vue. added transactions.

--- src/App.vue
+++ src/App.vue
@@ -1,30 +1,68 @@
 <script setup>
-import HelloWorld from './components/HelloWorld.vue'
+import Header from './components/Header.vue';
+import Balance from './components/Balance.vue';
+
+
+  import {ref, computed} from 'vue'
+
+const transactions = ref([
+    {id: 1, text: 'Paycheck', amount: 700.00}
+    {id: 1, text: 'Water Bill', amount: -72.83}
+    {id: 1, text: 'Electric Bill', amount: -153.89}
+    {id: 1, text: 'Returned Item', amount: 20.00}
+
+
+])
+
+
+
+
+  const sum = computed(())
+
+
+
 </script>
 
+
 <template>
-  <div>
-    <a href="https://vitejs.dev" target="_blank">
-      <img src="/vite.svg" class="logo" alt="Vite logo" />
-    </a>
-    <a href="https://vuejs.org/" target="_blank">
-      <img src="./assets/vue.svg" class="logo vue" alt="Vue logo" />
-    </a>
-  </div>
-  <HelloWorld msg="Vite + Vue" />
+
+<Header></Header>
+
+<div class="container">
+  <Balance :total="1000"></Balance>
+
+
+</div>
+
+
 </template>
 
-<style scoped>
-.logo {
-  height: 6em;
-  padding: 1.5em;
-  will-change: filter;
-  transition: filter 300ms;
-}
-.logo:hover {
-  filter: drop-shadow(0 0 2em #646cffaa);
-}
-.logo.vue:hover {
-  filter: drop-shadow(0 0 2em #42b883aa);
-}
-</style>
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
